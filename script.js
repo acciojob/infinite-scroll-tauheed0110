dcd8b6968d743a922621765e3be6b4596e1390b1
tauheed0110 <tauheed0110@gmail.com>
2024-11-07 10:53:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-//your code here!
+const main = document.getElementById("infi-list");
+// making a function that adds two item
+function addItem(){
+    const lastNumber = Number(main.lastElementChild.innerHTML.split(" ")[2]);
+    const newElement1 = document.createElement("li");
+    const newElement2 = document.createElement("li");
+    newElement1.textContent = `List item ${lastNumber+1}`;
+    newElement2.textContent = `List item ${lastNumber+2}`;
+    main.appendChild(newElement1);
+    main.appendChild(newElement2);
+}
 
+
+main.addEventListener("scroll", ()=>{
+    if(main.scrollTop + main.clientHeight >= main.scrollHeight){
+        addItem();
+    }
+})
